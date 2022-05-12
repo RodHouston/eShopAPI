988ae02827b56787e9c58167f8f48781174f3bed
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-05-12 12:40:12 -0400

    try deploy

--- src/api.js
+++ src/api.js
@@ -29,19 +29,9 @@ router.get("/", async (req, res) => {
   const qNew = req.query.new;
   const qCategory = req.query.category;
   try {
-    let products;
-
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: - 1 }).limit(1);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      });
-    } else {
+  
       products = await Product.find();
-    }
+   
 
     res.status(200).json(products);
   } catch (err) {
