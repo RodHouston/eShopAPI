a109d2a9ec86753c9581c2aa529799f77ba9fe62
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-05-12 11:29:25 -0400

    try deploy

--- src/api.js
+++ src/api.js
@@ -2,6 +2,29 @@ const express = require("express");
 const serverless = require("serverless-http");
 const productRoute = require('../routes/product.js')
 
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user.js')
+const authRoute = require('./routes/auth.js')
+const productRoute = require('./routes/product.js')
+const cartRoute = require('./routes/cart.js')
+const orderRoute = require('./routes/order.js')
+const stripeRoute = require('./routes/stripe.js')
+
+const cors = require("cors")
+
+dotenv.config()
+
+mongoose.connect( process.env.MONGO_URL )
+.then(() => console.log('connected'))
+.catch((err) => {
+    console.log(err);
+} )
+
+app.use(express.json())
+
+app.use(cors()); 
+
 const app = express();
 const router = express.Router();
 
@@ -23,11 +46,25 @@ router.post('/testpost',(req,res) => {
         hello: "hit the POST!"
       });
 })
-c
 
+
+app.use('/.netlify/functions/api/users', userRoute)
+
+app.use('/.netlify/functions/api/auth', authRoute)
 
 app.use('/.netlify/functions/api/products', productRoute)
+
+app.use('/.netlify/functions/api/carts', cartRoute)
+
+app.use('/.netlify/functions/api/orders', orderRoute)
+
+app.use('/.netlify/functions/api/checkout', stripeRoute)
+
+
 app.use(`/.netlify/functions/api`, router);
 
+app.listen(process.env.PORT || 3008, () => {
+  console.log('listening on port:' + process.env.PORT);
+})
 module.exports = app;
 module.exports.handler = serverless(app);
