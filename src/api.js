b813a8d8f525a8ec1785f47c97d8ca463e6203a5
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-05-12 12:47:11 -0400

    try deploy

--- src/api.js
+++ src/api.js
@@ -28,17 +28,13 @@ app.use(cors());
 router.get("/", async (req, res) => {
   const qNew = req.query.new;
   const qCategory = req.query.category;
-  try {
+
   
-      products = await Product.find();
+      products = Product.find();
    
 
     res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+})
 
 // router.get("/", (req, res) => {
 //   res.json({
